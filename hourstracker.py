e224e7ed8cbdb44c6672f51bc0b4f86df443a400
FosterLundwick <279334107+GothEngineering@users.noreply.github.com>
2026-06-27 09:09:04 -0400

    Add a basic UI to the script
    You close it with the X and it saves automatically, omg

--- hourstracker.py
+++ hourstracker.py
@@ -1,9 +1,12 @@
 import time
 import datetime
+import tkinter
 
 start_time = time.time()
 current_time = 0
 
+root = tkinter.Tk()
+root.title("Hours tracker")
 
 # Creating a file if there is not one
 try:
@@ -20,27 +23,43 @@ with open("horas", "r") as f:
     print(current_time)
     
 
-# The part where it does the writing
-while True:
-    try:
-        time.sleep(1)
+def close_app():
+    end_time = time.time()
+    finished_time = end_time - start_time
+    finished_time += float(current_time)
+
+    with open("horas", "w") as f:
+        f.write(str(finished_time))
+    
+    hours, remainder = divmod(finished_time, 3600)
+    minutes, remainder = divmod(remainder, 60)
+    root.destroy()
+
+# The part where it does the writing to the file every 1 second
+#while True:
+    #try:
+        #time.sleep(1)
 
 
-    except KeyboardInterrupt:
-        end_time = time.time()
+    #except KeyboardInterrupt:
+        #end_time = time.time()
 
         
-        finished_time = end_time - start_time
-        finished_time += float(current_time)
+        #finished_time = end_time - start_time
+        #finished_time += float(current_time)
 
 
         # Writes the final time to the notepad
-        with open("horas", "w") as f:
-            f.write(str(finished_time))
+        #with open("horas", "w") as f:
+            #f.write(str(finished_time))
 
         # Study more about divmod i still don't understand this
-        hours, remainder = divmod(finished_time, 3600)
-        minutes, remainder = divmod(remainder, 60)
-        print(f"{int(hours)} hours {int(minutes)} minutes")
+        #hours, remainder = divmod(finished_time, 3600)
+        #minutes, remainder = divmod(remainder, 60)
+        #print(f"{int(hours)} hours {int(minutes)} minutes")
 
-        break
+        #break
+
+
+root.protocol("WM_DELETE_WINDOW", close_app)
+root.mainloop()
