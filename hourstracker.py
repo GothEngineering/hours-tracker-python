d9e9e47c0d8d479c788e698d56b4fc05cd79164c
FosterLundwick <279334107+GothEngineering@users.noreply.github.com>
2026-07-03 19:24:23 -0400

    Working state prior to refactoring

--- hourstracker.py
+++ hourstracker.py
@@ -21,6 +21,7 @@ except FileExistsError:
 with open("horas", "r") as f:
     total_hours = f.read()
     current_time = total_hours
+    
     float_time = float(current_time)
     
     hours = float_time
@@ -28,15 +29,28 @@ with open("horas", "r") as f:
     #minutes, remainder = divmod(remainder, 60)
     hours_label = f"{hours}"
     
-    print(current_time)    
+    #print(current_time)    
+
+
+def float_to_int_converter(time):
+    hours, remainder = divmod(time, 3600)
+    minutes, remainder = divmod(remainder, 60)
+    #template = 
 
 
 def refresh_data():
+    # Please change this global variable for later, it can cause issues (supposedly)
+    global hours_label
     end_time = time.time()
     finished_time = end_time - start_time
     finished_time += float(current_time)
-    
-    hours_label.update()
+    hours_label = finished_time
+
+    ui_label.config(text=f"Time invested: {hours_label}")
+    print(f"Hours Label: {hours_label}, Current time: {current_time}")
+
+def start_tracking():
+    pass
 
 def close_app():
     end_time = time.time()
@@ -54,13 +68,16 @@ root.protocol("WM_DELETE_WINDOW", close_app)
 
 
 
-hours_label = tkinter.Label(root, text=f"Time invested: {hours_label}", bg="gray", fg="white")
-hours_label.pack(fill="both", expand=True)
+ui_label = tkinter.Label(root, text=f"Time invested: {hours_label}", bg="gray", fg="white")
+ui_label.pack(fill="both", expand=True)
 
 timer_start = tkinter.Button(root, text="Start", width=10, bg="gray")
 timer_start.pack(fill="both", expand=True)
 
 refresh_button = tkinter.Button(root, text="Refresh", width=5, bg="gray", command=refresh_data)
 refresh_button.pack(fill="both", expand=True)
+
+
+
 
 root.mainloop()
