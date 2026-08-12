0bd89927fb1b00a37d2ba9168814c33d305181a4
FosterLundwick <279334107+GothEngineering@users.noreply.github.com>
2026-08-12 17:14:40 -0400

    Add complete functionality to the pause button, alongside separating the UI code from the tracker logic
    OH heavens

--- hourstracker.py
+++ hourstracker.py
@@ -15,8 +15,8 @@ root.config(bg=background_color)
 
 
 # TO DO:
-# Lay the groundwork for sqlite3, find a way to pause the app, modernize it into customtkinter
-# im just kinda eepy
+# Lay the groundwork for sqlite3, modernize it into customtkinter
+# eff th belly ache
 
 class HoursTracker():
 
@@ -30,10 +30,14 @@ class HoursTracker():
         # This variable manages the label that shows the hours, it is used to turn the float into time
         self.hours_label = 0
 
-        #
+        # The variable that changes if the pause button is pressed
         self.is_time_paused = False
-        self.time_while_paused = 0
 
+        # Variable that stores the total amount paused so the tracker doesn't skip to the present after unpausing it
+        self.time_spent_paused = 0
+
+        # Just a check so Python doesn't throw an error 
+        self.time_ticking = None
 
         # Creating a text file if there is not one
         try:
@@ -68,17 +72,26 @@ class HoursTracker():
         self.pause_button.pack()
 
 
-    def update_ui(self):  
+    def tracking_hours(self):  
 
+        if self.is_time_paused:
+            self.time_spent_paused += 1
+            print(self.time_spent_paused)      
+        
         # This part here simply updates the time because it does this operation whenever I refresh
         # The finished_time value grows bigger because it adds the latest end_time and it simply adds it up to the current_time variable
+        # It substracts the time paused so it doesn't wake up and skips to the boring present
         end_time = time.time()
-        finished_time = end_time - self.start_time
-        finished_time += float(self.current_time)
+        self.finished_time = end_time - self.start_time - self.time_spent_paused
+        self.finished_time += float(self.current_time)
+
+        self.time_ticking = root.after(1000, self.tracking_hours)
+
+    def update_ui(self):
 
         # This part is JUST the data that goes into the label, it grabs the finished_time which is the latest value
         # and then it changes the float into hours and minutes
-        self.hours_label = finished_time
+        self.hours_label = self.finished_time
         hours_in_the_float = round(self.hours_label) // 3600
         seconds_without_hours = round(self.hours_label) % 3600
         minutes = seconds_without_hours // 60
@@ -86,20 +99,32 @@ class HoursTracker():
 
         self.ui_label.config(text=f"Time invested: {hours_in_the_float} hours, {minutes} minutes, {seconds_modulo} seconds.")
 
-        time_ticking = root.after(1000, self.update_ui)
-
-        if self.is_time_paused:
-            root.after_cancel(time_ticking)
+        self.updating_label = root.after(1000, self.update_ui)
 
         
 
+    def pause_timer(self):
+        self.is_time_paused = not self.is_time_paused
+
+        if self.is_time_paused:
+            self.pause_button.configure(text="Unpause")
+            root.after_cancel(self.updating_label)
+            
+            print(f"Tengo ahora mismo un {self.time_spent_paused}")
+
+        else:
+            self.pause_button.configure(text="Pause")
+            self.update_ui()
+            print(f"mreow ahora tengo {self.time_spent_paused}")
+
+
     def close_app(self):
         end_time = time.time()
-        finished_time = end_time - self.start_time
-        finished_time += float(self.current_time)
+        self.finished_time = end_time - self.start_time - self.time_spent_paused
+        self.finished_time += float(self.current_time)
 
         with open("hours", "w") as f:
-            f.write(str(finished_time))
+            f.write(str(self.finished_time))
     
 
         root.destroy()
@@ -107,20 +132,15 @@ class HoursTracker():
 
     def auto_save(self):
         end_time = time.time()
-        finished_time = end_time - self.start_time
-        finished_time += float(self.current_time)
+        self.finished_time = end_time - self.start_time - self.time_spent_paused
+        self.finished_time += float(self.current_time)
 
         with open("hours", "w") as f:
-            f.write(str(finished_time))
+            f.write(str(self.finished_time))
     
         root.after(120000, self.auto_save)
 
-    def pause_timer(self):
-        self.is_time_paused = not self.is_time_paused
-        if self.is_time_paused:
-            self.pause_button.configure(text="Unpause")
-        else:
-            self.pause_button.configure(text="Pause")
+
 
 
 tracker = HoursTracker()
@@ -128,6 +148,7 @@ tracker = HoursTracker()
 
 root.protocol("WM_DELETE_WINDOW", tracker.close_app)
 
-root.after(1000, tracker.update_ui)
+root.after(500, tracker.tracking_hours)
+root.after(500, tracker.update_ui)
 root.after(60000, tracker.auto_save)
 root.mainloop()
